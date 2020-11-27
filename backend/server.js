7b506622fe0d442c96abb4b7f9a62c878effc57a
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-27 18:48:00 -0500

    add user router

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,8 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(cors());
+// set up routes
+app.use("/users", require("./routers/userRouter"));
 
 app.listen(PORT, () => {
   console.log("server is running on port ", PORT);
