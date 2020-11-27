0050d8620ebd732494c89675709269530f7d9323
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-27 18:39:50 -0500

    add user model

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,27 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = process.env.PORT;
+
+// DATABASE
+const db = mongoose.connection;
+const mongoURI = process.env.mongoURI;
+
+mongoose.connect(
+  mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("👉🏼The connection with mongod is established🤟🏼🎼");
+  }
+);
+// database error checks
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 const app = express();
 // middleware
