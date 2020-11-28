01eb46930fa9a0ecda9002ac7ec3c043aec73263
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-27 21:30:24 -0500

    add header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Header from "./components/layout/Header";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
@@ -8,6 +9,7 @@ export default class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <Switch>
           <Route exact path={"/"} component={Home} />
           <Route exact path={"/login"} component={Login} />
