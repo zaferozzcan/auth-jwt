af052a01074156a909198ea5f6be0039ae920681
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-27 23:48:58 -0500

    add register form

--- client/src/components/auth/AuthOptions.jsx
+++ client/src/components/auth/AuthOptions.jsx
@@ -17,22 +17,38 @@
 //   );
 // }
 
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../../context/UserContext";
 
 export default function AuthOptions() {
+  const { userData, setUserData } = useContext(UserContext);
+
+  function handleLogOut() {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    localStorage.setItem("auth-token", "");
+  }
+
   return (
     <>
       <nav className="auth-options">
-        <Link to={"/"}>
-          <button>Log Out</button>
-        </Link>
-        <Link to={"/register"}>
-          <button>Register</button>
-        </Link>
-        <Link to={"/login"}>
-          <button>Log In</button>
-        </Link>
+        {userData.user ? (
+          <Link onClick={handleLogOut} to={"/"}>
+            <button>Log Out</button>
+          </Link>
+        ) : (
+          <>
+            <Link to={"/register"}>
+              <button>Register</button>
+            </Link>
+            <Link to={"/login"}>
+              <button>Log In</button>
+            </Link>
+          </>
+        )}
       </nav>
     </>
   );
