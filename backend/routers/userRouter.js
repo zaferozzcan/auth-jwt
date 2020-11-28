6264fc120f31b0dbde764870c272e78a83029ebf
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-27 19:05:47 -0500

    add some validation for login

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,35 @@
 const router = require("express").Router();
+const User = require("../models/userModel");
 
-router.get("/test", (req, res) => {
-  res.send("hello it is working");
+router.post("/register", async (req, res) => {
+  try {
+    // check if all areas are filled
+    let { email, password, passwordCheck, displayName } = req.body;
+
+    if (!email || !password || !passwordCheck)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
+    if (password.length < 5)
+      return res.status(400).json({
+        msg: "The password needs to be at least 5 characters long.",
+      });
+    if (password !== passwordCheck)
+      return res
+        .status(400)
+        .json({ msg: "Enter the same password twice for verification." });
+
+    // check existing user
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exists." });
+
+    // check a name entered
+    if (!displayName) displayName = email; // display email if a name is not given by user
+  } catch (err) {
+    if (err) {
+      res.status(500).json(err);
+    }
+  }
 });
-
 module.exports = router;
