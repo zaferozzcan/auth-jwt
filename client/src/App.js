5605ca24896ff1539f9043c032178523c972c407
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-27 23:12:54 -0500

    get get token and store it

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from "react";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Header from "./components/layout/Header";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
 
-export default class App extends Component {
-  render() {
-    return (
+export default function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:3030/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenRes.data) {
+        const userRes = await Axios.get("http://localhost:3030/users/", {
+          headers: { "x-auth-token": token },
+        });
+        console.log(tokenRes.data);
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
+  return (
+    <div>
       <>
-        <Header />
-        <Switch>
-          <Route exact path={"/"} component={Home} />
-          <Route exact path={"/login"} component={Login} />
-          <Route exact path={"/register"} component={Register} />
-        </Switch>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Header />
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/login"} component={Login} />
+            <Route exact path={"/register"} component={Register} />
+          </Switch>
+        </UserContext.Provider>
       </>
-    );
-  }
+    </div>
+  );
 }
